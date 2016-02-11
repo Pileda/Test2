0bfd71429b1abdf17c79c6ff13283e7f0966c705
Quentin van Kesteren <quentin.vankesteren.w4012@student.hogent.be>
2016-02-11 13:00:34 +0100

    things shall change!

---
+++ src/domein/Testhaha.java
@@ -0,0 +1,11 @@
+
+package domein;
+
+
+public class Testhaha {
+    private String zeBest;
+    
+    public String testHaha(){
+       return "hopelijk werkt dees omdat jullie allemaal homo's zijn!";
+    }
+}
